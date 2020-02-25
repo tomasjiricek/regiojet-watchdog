3e405df537157e46a31b8fb6084f1f822c982570
tomasjiricek <tjiricek@email.cz>
2020-02-25 23:17:30 +0100

    add compression to service

--- src/server/service/index.js
+++ src/server/service/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const apiRouter = require('./apiRouter');
+const compression = require('compression');
 
 const app = express();
 
 function initApp(isDevMode = false) {
+    app.use(compression());
     app.all('*', (_, res, next) => {
         res.header('Access-Control-Allow-Origin', '*');
         res.header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
