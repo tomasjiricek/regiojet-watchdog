2f76a6abd337669af2d7d7d09cef34f8cbce4592
Tomas Jiricek <tjiricek@email.cz>
2020-03-14 16:27:57 +0100

    rewrite www service to load certificate from path given in config

--- src/www.js
+++ src/www.js
@@ -7,10 +7,6 @@ const path = require('path');
 const service = require('./server/service');
 
 const DATA_DIR = path.join(__dirname, '../data');
-const SSL_CERT_DIR = path.join(DATA_DIR, 'sslcert');
-
-const privateKey  = fs.readFileSync(path.join(SSL_CERT_DIR, 'server.key'), 'utf8');
-const certificate = fs.readFileSync(path.join(SSL_CERT_DIR, 'server.crt'), 'utf8');
 
 let config;
 
@@ -19,7 +15,7 @@ argv.option([
         name: 'config',
         short: 'c',
         type: 'string',
-        description: 'Optional config path. Default path is ./data/config.json',
+        description: 'Optional config path. Default path is ./data/config.js',
         example: 'node www --config=./config.json'
     }, {
         name: 'port',
@@ -42,22 +38,39 @@ try {
     if (args.config) {
         config = require(args.config);
     } else {
-        config = require(path.join(DATA_DIR, 'config.json'));
+        config = require(path.join(DATA_DIR, 'config.js'));
     }
 } catch (e) {
-    console.log(colors.red('Config error:', e.message));
+    console.error(colors.red('Config error:', e.message));
+    return;
 }
 
-if (config) {
-    const port = args.port || config.listenOnPort || 3333;
+const port = args.port || config.listenOnPort;
 
-    try {
-        service.initApp(args.devmode);
-        const server = https.createServer({ key: privateKey, cert: certificate }, service.app);
-        server.listen(port);
-        console.log(colors.cyan(`\t---- Service is running ${args.devmode ? '(in dev mode) ' : ''}----`));
-        console.log(colors.green(`\n\tURL: https://localhost:${port}`));
-    } catch (e) {
-       console.log(colors.red('Error:', e.message));
-    }
+if (!port) {
+    console.error(colors.red('No server port specified!'));
+    return;
+}
+
+const { certificatePath } = config;
+
+if (!(certificatePath instanceof Object) || !certificatePath.crt || !certificatePath.key) {
+    console.error(colors.red('Config is missing "certificatePath" property or the property is not object containing "crt" and "key" properties'));
+    return;
+}
+
+const { crt: crtPath, key: keyPath } = certificatePath;
+
+try {
+    service.initApp(args.devmode);
+    const server = https.createServer({
+        key: fs.readFileSync(keyPath, 'utf8'),
+        cert: fs.readFileSync(crtPath, 'utf8')
+    }, service.app);
+    server.listen(port);
+
+    console.log(colors.cyan(`\t---- Service is running ${args.devmode ? '(in dev mode) ' : ''}----`));
+    console.log(colors.green(`\n\tURL: https://localhost:${port}`));
+} catch (e) {
+    console.error(colors.red('Error:', e));
 }
