927bca85876c0cf8a40974b0bce15fa261ce1598
Tomas Jiricek <tjiricek@email.cz>
2020-03-16 11:15:14 +0100

    add test script for posting notifications

---
+++ post.js
@@ -0,0 +1,26 @@
+var webpush = require('web-push');
+const secrets = require('./data/secrets.json');
+const subscriptions = require('./data/web-push-subscriptions.json');
+
+var payload = 'Test message triggered manually by Node.JS script';
+
+var options = {
+  TTL: 60
+};
+
+const token = process.argv.slice(2).join(' ');
+if (!token) {
+    return console.error('No token passed');
+}
+
+webpush.setVapidDetails(
+  'mailto:tjiricek11@gmail.com',
+  secrets.webPushPublicKey,
+  secrets.webPushPrivateKey
+);
+
+subscriptions[token].subscriptions.forEach((subscription) => {
+    webpush.sendNotification(subscription, payload, options)
+        .then((d) => { console.log(d); })
+        .catch((e) => { console.error(e); });
+});
