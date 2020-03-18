6ebe47f4a70fb7f227a1d6c82ea49a1ae1be3cad
Tomas Jiricek <tjiricek@email.cz>
2020-03-18 01:05:38 +0100

    fix wording in about section

--- src/client/components/About.jsx
+++ src/client/components/About.jsx
@@ -4,10 +4,10 @@ import Content from './Content';
 
 const About = () => (
     <Content style={{ backgroundColor: 'transparent', margin: '0 20px' }}>
-        <h3>Tato aplikace není oficiální a není provozovaná společností RegioJet&nbsp;a.s.</h3>
+        <h3>Tato aplikace není provozovaána společností RegioJet&nbsp;a.s.</h3>
         <p>
-            Aplikace slouží pro vyhledávání a hlídání spojů této společnosti.
-            Není určená pro širší veřejnost, proto ji prosím nikde <strong>nesdílejte</strong>.
+            Aplikace slouží pro vyhledávání a hlídání spojů společnosti RegioJet.
+            Není určena pro širší veřejnost, proto ji prosím nikde <strong>nesdílejte</strong>.
         </p>
     </Content>
 );
