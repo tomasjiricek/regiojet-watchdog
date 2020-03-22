86d1e8a3e01114edbacb08456438a9097da68834
tomasjiricek <tjiricek@email.cz>
2020-03-22 02:50:15 +0100

    add sound prop for testing purposes

--- res/static/sw.js
+++ res/static/sw.js
@@ -61,6 +61,7 @@ self.addEventListener('push', (event) => {
         icon: '/static/images/icons/icon-256.png',
         tag: 'rj-watchdog-id',
         renotify: true,
+        sound: 'default',
         vibrate: [
             2,1,4,1,6,1,8,1,10,1,20,1,2,1,4,1,6,1,8,1,10,1,14,1,18,1,40,40,
             2,1,4,1,6,1,8,1,10,1,20,1,2,1,4,1,6,1,8,1,10,1,14,1,18,1,40,40,
