e28b510b17ea3a485e621e6443fad313b1cd51b9
Tomas Jiricek <tjiricek@email.cz>
2020-03-26 03:27:56 +0100

    remove unused path

--- src/common/constants.js
+++ src/common/constants.js
@@ -3,7 +3,6 @@ const path = require('path');
 const DATA_PATH = path.resolve(__dirname, '../../data');
 
 const PATHS = {
-    AUTHORIZED_DEVICES: path.join(DATA_PATH, 'authorized-devices.json'),
     DB_STORAGE_PATH: path.join(DATA_PATH, 'db-storage.sqlite'),
     SECRETS: path.join(DATA_PATH, 'secrets.json'),
     USERS: path.join(DATA_PATH, 'users.json'),
