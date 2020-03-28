003fe20a0cb49fc2544634231842339aa61b0142
Tomas Jiricek <tjiricek@email.cz>
2020-03-28 23:33:56 +0100

    rewrite user.js to use sqlite db

--- src/server/utils/user.js
+++ src/server/utils/user.js
@@ -1,30 +1,28 @@
 const crypto = require('crypto');
-const fs = require('fs');
+const sqlite = require('sqlite3');
 
 const { PATHS } = require('../../common/constants');
 
-function createUsersFile() {
-    fs.writeFile(PATHS.USERS, JSON.stringify({}), () => {});
-}
+const db = new sqlite.Database(PATHS.DB_STORAGE_PATH);
+
+const SQL = {
+    GET_USER_DATA: 'SELECT token, device_id AS deviceId FROM users WHERE token = ?',
+    REGISTER: 'INSERT INTO users (token, device_id) VALUES (?, ?)'
+};
 
 function findUserByToken(token) {
     return new Promise((resolve, reject) => {
-        fs.readFile(PATHS.USERS, {}, (err, data) => {
+        db.get(SQL.GET_USER_DATA, [token], (err, row) => {
             if (err) {
-                createUsersFile();
+                console.error('Failed to get user data by token:', err);
+                reject({ code: 500, message: 'Faield to find user in database' });
+                return;
+            }
+            if (!row) {
                 reject({ code: 403, message: 'User not found' });
                 return;
             }
-            try {
-                const users = JSON.parse(data);
-                if (users[token]) {
-                    resolve(users[token]);
-                    return;
-                }
-            } catch (_) {
-                createUsersFile();
-            }
-            reject({ code: 403, message: 'User not found' });
+            resolve(row);
         });
     });
 }
@@ -36,7 +34,7 @@ function getRandomDeviceId() {
 function getUserByAuthData(authData) {
     return new Promise((resolve, reject) => {
         if (!isAuthDataValid(authData)) {
-            reject({ code: 500, message: 'Invalid data' });
+            reject({ code: 400, message: 'Invalid data structure' });
             return;
         }
 
@@ -59,44 +57,32 @@ function isAuthDataValid({ selectedImageIndex = null, size = null, pattern = nul
 function registerUser(authData) {
     return new Promise((resolve, reject) => {
         if (!isAuthDataValid(authData)) {
-            reject({ code: 500, message: 'Invalid data' });
+            reject({ code: 400, message: 'Invalid data structure' });
             return;
         }
 
         const { selectedImageIndex, size, pattern } = authData;
         const data = `C${pattern.join(':')}S${size}I${selectedImageIndex}`;
         const token = crypto.createHmac('sha256', 'RJ').update(data).digest('hex');
+        const deviceId = getRandomDeviceId();
 
-        fs.readFile(PATHS.USERS, {}, (err, data) => {
-            if (err) {
-                return;
-            }
-
-            let users = {};
-
-            try {
-                users = JSON.parse(data);
-            } catch (_) {}
-
-            if (users[token] !== undefined) {
-                reject({ code: 500, message: 'Failed to register the user.'});
-                return;
-            }
-
-            users[token] = {
-                deviceId: getRandomDeviceId(),
-                registered: new Date().getTime(),
-                token
-            };
-
-            fs.writeFile(PATHS.USERS, JSON.stringify(users), (err) => {
-                if (err) {
-                    reject({ code: 500, message: 'Failed to register the user. Try again later.' });
+        findUserByToken(token)
+            .then(() => reject({ code: 500, message: 'Failed to register the user.' }))
+            .catch((err) => {
+                if (err.code === 500) {
+                    reject({ code: 500, message: 'Failed to register the user, please try again later.' });
                     return;
                 }
-                resolve(users[token]);
+
+                db.run(SQL.REGISTER, [token, deviceId], (err) => {
+                    if (err) {
+                        console.error('Failed to register user:', err);
+                        reject({ code: 500, message: 'Failed to register the user, please try again later.' });
+                        return;
+                    }
+                    resolve({ token, deviceId });
+                });
             });
-        });
     });
 }
 
